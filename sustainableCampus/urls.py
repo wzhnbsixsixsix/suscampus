8a47af4bca57d4b653e6f4642b7674c4e6b66cc7
GeorgiySav <gesav@live.com>
2025-02-13 13:55:06 +0000

    Added place holder home page to implement template html files

--- sustainableCampus/urls.py
+++ sustainableCampus/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import sustainableCampus.views as views
 
 urlpatterns = [
     path("main/", include("main.urls")),
     path('admin/', admin.site.urls),
+    path('', views.home, name='home')
 ]
