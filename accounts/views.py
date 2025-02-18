5f7614b900e29ba6ce9a368fb6ccda5edf7b12f8
StickyDogShit <w2hnb666@163.com>
2025-02-18 12:43:52 +0000

    signupform->SignUpForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,10 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
-from .forms import signUpForm, loginForm
+from .forms import SignUpForm, LoginForm
 from django.contrib.auth import logout
 from django.core.mail import send_mail
 from django.conf import settings
-from .forms import SignUpForm, LoginForm
 from .models import CustomUser
 
 # Create your views here.
