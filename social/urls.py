87ca8a1edc262b476892c2c23ea930702e655c82
16apaul <86714317+16apaul@users.noreply.github.com>
2025-02-19 11:45:51 +0000

    Delete social directory

--- social/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.announcement_list, name='announcement_list'),
-    path('create/', views.create_announcement, name='create_announcement'),
-]
