f96de94406f74250894b578761ef0c298ec5b65b
16apaul <86714317+16apaul@users.noreply.github.com>
2025-02-19 11:44:35 +0000

    Add files via upload

---
+++ announcements/forms.py
@@ -0,0 +1,9 @@
+# forms.py
+
+from django import forms
+from .models import Announcement
+
+class AnnouncementForm(forms.ModelForm):
+    class Meta:
+        model = Announcement
+        fields = ['title', 'content']
