eb4e6096075ef2052aa771304b9284fd9af1f1b1
16apaul <86714317+16apaul@users.noreply.github.com>
2025-02-19 11:50:02 +0000

    Add files via upload

---
+++ announcements/views.py
@@ -0,0 +1,21 @@
+# views.py
+
+from django.shortcuts import render, redirect
+from .models import Announcement
+from .forms import AnnouncementForm
+
+def announcement_list(request):
+    announcements = Announcement.objects.all().order_by('-created_at')
+    return render(request, 'announcements/announcement_list.html', {'announcements': announcements})
+
+def create_announcement(request):
+    if request.method == 'POST':
+        form = AnnouncementForm(request.POST)
+        if form.is_valid():
+            new_announcement = form.save(commit=False)
+            new_announcement.author = request.user
+            new_announcement.save()
+            return redirect('announcement_list')
+    else:
+        form = AnnouncementForm()
+    return render(request, 'announcements/create_announcement.html', {'form': form})
