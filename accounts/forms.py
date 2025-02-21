efb776ec722f292adf360e57b00faca17c858cb1
StickyDogShit <w2hnb666@163.com>
2025-02-21 21:36:15 +0000

    add debug msg for signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,19 @@ class SignUpForm(UserCreationForm):
         model = CustomUser
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address has been used, please use another email address.")
+        return email
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            self.add_error("password2", "The passwords entered twice are inconsistent.")
+        return cleaned_data
 class LoginForm(AuthenticationForm):
     pass
 
