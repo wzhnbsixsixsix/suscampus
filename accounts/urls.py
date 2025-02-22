60f695eda673d0ab2e164008deacda0186f2f9c1
StickyDogShit <w2hnb666@163.com>
2025-02-22 23:25:54 +0000

    the change name function is work but change passoword isn't

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import change_password
 
 app_name = 'accounts'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path("signup/", views.signup_page, name="signup"),
     path("login/", views.login_page, name="login"),
     path("email_verification/<uuid:token>", views.email_verification, name="email_verification"),
-    path("profile/",views.setting_page,name="profile"),
+    path("profile/", views.profile_page, name="profile"),
+    path('change_username/', views.change_username, name='change_username'),
+    path('change-password/', change_password, name='change_password'),
 ]
-
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login
-from .forms import SignUpForm, LoginForm
+from django.contrib.auth import authenticate, login, update_session_auth_hash
+from .forms import SignUpForm, LoginForm, ChangeUsernameForm
 from django.contrib.auth import logout
 from django.core.mail import send_mail
 from django.conf import settings
 from .models import CustomUser
 from shop.models import UserBalance
+
 
 # Handles data submitted from signup page's form
 def signup_page(request):
@@ -18,14 +22,14 @@ def signup_page(request):
         if form.is_valid():
             # Checks if given email is unique before saving new user data
             if CustomUser.objects.filter(email=form.cleaned_data["email"]).exists():
-                    form.add_error("email", "Email is already in use.")
+                form.add_error("email", "Email is already in use.")
             # Saves new user data, and sends email verification
             else:
                 user = form.save()
                 send_email_verification(user)
                 return redirect('accounts:login')
 
-    context = {'form':form}
+    context = {'form': form}
     return render(request, 'accounts/signup.html', context)
 
 
@@ -33,8 +37,8 @@ def signup_page(request):
 def send_email_verification(user):
     subject = "Email Verification for Sustainable Campus"
     link = f"http://127.0.0.1:8000/accounts/email_verification/{user.verification_token}"
-    message =f"Hello {user.first_name}, \n\nPlease verify your email through the following link below:\n{link}\n\nThank You!"
-    sender = settings.EMAIL_HOST_USER # Sender of email is stored in settings.py
+    message = f"Hello {user.first_name}, \n\nPlease verify your email through the following link below:\n{link}\n\nThank You!"
+    sender = settings.EMAIL_HOST_USER  # Sender of email is stored in settings.py
     receiver = [user.email]
 
     send_mail(subject, message, sender, receiver)
@@ -62,6 +66,7 @@ def email_verification(request, token):
 
     return redirect('accounts:login')
 
+
 # Handles data submitted by login page's form
 def login_page(request):
     form = LoginForm(request)
@@ -76,9 +81,9 @@ def login_page(request):
 
             # Retrieves user data for matching username and password
             user = authenticate(request, username=username, password=password)
-            
+
             # Checks if user data has been retrieved
-            if user is not None: 
+            if user is not None:
                 # Checks if user is verified
                 if user.verified == True:
                     login(request, user)
@@ -89,12 +94,12 @@ def login_page(request):
 
             else:
                 form.add_error(None, 'Invalid username or password')
-            
+
         else:
-            context = {'form':form}
+            context = {'form': form}
             return render(request, 'accounts/login.html', context)
     else:
-        context = {'form':form}
+        context = {'form': form}
         return render(request, 'accounts/login.html', context)
 
 
@@ -103,5 +108,49 @@ def logout_view(request):
     messages.success(request, "Log out successfully")
     return redirect("accounts:login")
 
-def setting_page(request):
-    return render(request, 'accounts/profile.html')
+
+@login_required  # Make sure the user is logged in
+def profile_page(request):
+    username = request.user.username
+    email = request.user.email
+    context = {
+        'username': username,
+        'email': email,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+def change_username(request):
+    if request.method == 'POST':
+        form = ChangeUsernameForm(request.POST, instance=request.user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+            messages.success(request, "Username modification was successful!")
+            return redirect('accounts:profile')# Redirect when successful
+        # When verification fails, do not jump and directly render the current page
+        return render(request, 'accounts/profile.html', {
+            'form': form,  #Form containing error message
+            'show_modal': True
+        })
+    else:
+        form = ChangeUsernameForm(instance=request.user)
+
+    return render(request, 'accounts/profile.html', {
+        'form': form,
+        'show_modal': True
+    })
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request, 'Your password has been updated successfully!')
+            return redirect('accounts:profile')
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = PasswordChangeForm(request.user)
+
+    return render(request, 'accounts/change_password.html', {'form': form})
