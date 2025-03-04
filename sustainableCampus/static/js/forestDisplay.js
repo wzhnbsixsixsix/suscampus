8dc3d8deb8036dd362a9f8a5d05041891b1ffe1f
Jess <jessicaclarridge@gmail.com>
2025-03-04 16:15:43 +0000

    added simple grid generation to the forest page

---
+++ sustainableCampus/static/js/forestDisplay.js
@@ -0,0 +1,23 @@
+const forestView = document.getElementById("forest-grid");
+
+function onCellClick() {
+    console.log("cell clicked");
+}
+
+// generate user's forest grid 
+function generateGrid(rows, cols) {
+    forestView.style.setProperty('--forest-rows', rows);
+    forestView.style.setProperty('--forest-cols', cols);
+    // iterates for each cell that will be in the grid
+    for (let i = 0; i < (rows*cols); i++) {
+        let gridCell = document.createElement("div");
+        console.log("Creating forest-cell-" + i);
+        const addedCell = forestView.appendChild(gridCell);
+        addedCell.className = "grid-item";
+        addedCell.id = "forest-cell-" + i;
+        console.log("Adding event listener for click");
+        addedCell.addEventListener("click", onCellClick)
+    }
+}
+
+generateGrid(4, 4);
