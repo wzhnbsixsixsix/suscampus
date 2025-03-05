ea53a4e210e3bf6824a63d7b97059e3b59945efb
16apaul <86714317+16apaul@users.noreply.github.com>
2025-03-05 22:17:08 +0000

    Fixed create announcemet html and cards now blow up when clicked in the announcements page also fixed an issue where the post where not sorted by most recent

--- announcements/admin.py
+++ announcements/admin.py
@@ -4,5 +4,5 @@ from .models import Announcement
 @admin.register(Announcement)
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created_at')  # Fields to display
-    list_editable = ('created_at',)  # Make created_at editable
+   # list_editable = ('created_at',)  # Make created_at editable
     search_fields = ('title', 'summary', 'content')  # Make the title and content searchable
