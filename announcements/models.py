727d00fc9f25f4eb48fe40706d598f33f822276d
Riley Mugford <rmrmugford@gmail.com>
2025-03-05 14:39:06 +0000

    Created most of the backend code needed for rewarding players for attending events, using QR code made by gamekeeper or developer

--- announcements/models.py
+++ announcements/models.py
@@ -1,4 +1,4 @@
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # models.py
 
 from django.db import models  
@@ -9,7 +9,7 @@ class Announcement(models.Model):
     summary = models.TextField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='announcement_images/', null=True, blank=True)  # Image field
-
+    is_event = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
 
@@ -18,5 +18,15 @@ class Announcement(models.Model):
 
     def get_author_role(self):
         return self.author.role  
+    
+class Event(models.Model):
+    announcement = models.OneToOneField(Announcement, on_delete=models.CASCADE)
+    currency_reward = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(300)])
+    transaction_description = models.TextField(max_length=200)
+    event_date = models.DateField()
+    event_code = models.TextField(max_length=32)
 
 
+class EventAttended(models.Model):
+    player = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
