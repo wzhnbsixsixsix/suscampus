64025cea963551123ba058cc34ee767317932710
StickyDogShit <w2hnb666@163.com>
2025-03-07 12:48:04 +0000

    suggession backend

--- suggestions/models.py
+++ suggestions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.db import models
+class Suggestion(models.Model):
+    CATEGORY_CHOICES = [
+        ('location', 'Add a location'),
+        ('bug', 'Bug report'),
+        ('other', 'Other'),
+    ]
 
-# Create your models here.
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
