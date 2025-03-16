a0ef75bb55480d386ad56bebeae9f4377ed2a5cf
Riley Mugford <rmrmugford@gmail.com>
2025-03-16 14:23:36 +0000

    improvements made. Include better comments, more tests, added additionals views needed.

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.suggestions, name="suggestions"),
-    # 在urlpatterns中添加
     path('view-suggestions/', views.view_suggestions, name='view_suggestions'),
     path('delete-suggestion/<int:suggestion_id>/', views.delete_suggestion, name='delete_suggestion'),
 ]
