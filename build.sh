3d54038d6452e0ee86e037f6aff78f021282daa0
StickyDogShit <w2hnb666@163.com>
2025-03-23 22:30:21 +0000

    password update

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$RENDER" = "true" ]; then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'abc')" | python manage.py shell
-fi
+
 
 # Exit on error
 set -o errexit
@@ -15,3 +13,8 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+
+if [ "$RENDER" = "true" ]; then
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'abc')" | python manage.py shell
+fi
