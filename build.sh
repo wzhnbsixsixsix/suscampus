e7c91051d070ed4e68dda3bcb6447f786162fb94
StickyDogShit <w2hnb666@163.com>
2025-03-23 22:29:04 +0000

    password update

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "$RENDER" = "true" ]; then
-  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_PASSWORD')" | python manage.py shell
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'abc')" | python manage.py shell
 fi
 
 # Exit on error
