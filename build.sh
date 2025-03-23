098d7583f9ebdec4e400ad4f6fdc2320184f9916
StickyDogShit <w2hnb666@163.com>
2025-03-23 22:22:38 +0000

    superuser

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+if [ "$RENDER" = "true" ]; then
+  echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_PASSWORD')" | python manage.py shell
+fi
+
 # Exit on error
 set -o errexit
 
